785462f7af0fb0f51f0d5f738caf61e170041a0f
clara-aurora <clara.aurora92@gmail.com>
2017-07-21 17:05:32 +1000

    attempt save temp dataframe

--- 20170613_TrialScriptReadInData.R
+++ 20170613_TrialScriptReadInData.R
@@ -8,10 +8,10 @@ Station_id <- hrs_stations$station_number
 
 Data1 <- vector()
 for (i in 1:length(Station_id)){ 
-  Data1[i] <- which(grepl(Station_id[i], dir("Z:/Public/BOM_Stationdata/MergedUpToDateFiles"))) #for the first station
+  Data1[i] <- which(grepl(Station_id[i], dir("Y:/Public/BOM_Stationdata/MergedUpToDateFiles"))) #for the first station
 }
 
-Dir1 <- dir("Z:/Public/BOM_Stationdata/MergedUpToDateFiles")
+Dir1 <- dir("Y:/Public/BOM_Stationdata/MergedUpToDateFiles")
 
 Dir2 <- Dir1[Data1]
 
@@ -19,7 +19,7 @@ MaxT <- list()
 St_Rain <- list()
 
 for (i in 1:length(Station_id)) {
-  data <- read.csv(paste("Z:/Public/BOM_Stationdata/MergedUpToDateFiles",Dir2[i],sep="/")) 
+  data <- read.csv(paste("Y:/Public/BOM_Stationdata/MergedUpToDateFiles",Dir2[i],sep="/")) 
   Dates <- paste(data$Year,data$Month,data$Day, sep="-")
   MaxT[[i]] <- window(zoo(data[,10],order.by=as.Date(Dates)), start = as.Date("1970-01-01"), end = as.Date("2010-12-31"))
   St_Rain[[i]]<- window(zoo(data[,6],order.by=as.Date(Dates)), start = as.Date("1970-01-01"), end = as.Date("2010-12-31"))  # use window() to cut the right time frame
@@ -27,11 +27,19 @@ for (i in 1:length(Station_id)) {
 
 # combine station rainDir1
 
+MaxT[[1]]
+
 test <- do.call(rbind, MaxT)
 names(test) <- Station_id
 
-lapply(MaxT, merge)
+sapply(MaxT, length)
+
+test<-do.call(merge, MaxT)
+saveRDS(MaxT, 'MaxT.Rds')
+
 tail(MaxT[[1]])
 
 for(i in 1:length(St_Rain)) z <- merge(z, get(St_Rain[[i]]))
 names(z) <- unlist()
+
+
