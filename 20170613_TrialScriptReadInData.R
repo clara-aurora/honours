17e3ee98c0c16af6d2bc79b772b5479839af6c7a
Clara da Costa-Reidel <clara.aurora92@gmail.com>
2017-07-25 10:14:32 +1000

    consolidating rainfall data. problem with column names

--- 20170613_TrialScriptReadInData.R
+++ 20170613_TrialScriptReadInData.R
@@ -8,10 +8,10 @@ Station_id <- hrs_stations$station_number
 
 Data1 <- vector()
 for (i in 1:length(Station_id)){ 
-  Data1[i] <- which(grepl(Station_id[i], dir("Y:/Public/BOM_Stationdata/MergedUpToDateFiles"))) #for the first station
+  Data1[i] <- which(grepl(Station_id[i], dir("Z:/Public/BOM_Stationdata/MergedUpToDateFiles"))) #for the first station
 }
 
-Dir1 <- dir("Y:/Public/BOM_Stationdata/MergedUpToDateFiles")
+Dir1 <- dir("Z:/Public/BOM_Stationdata/MergedUpToDateFiles")
 
 Dir2 <- Dir1[Data1]
 
@@ -19,18 +19,19 @@ MaxT <- list()
 St_Rain <- list()
 
 for (i in 1:length(Station_id)) {
-  data <- read.csv(paste("Y:/Public/BOM_Stationdata/MergedUpToDateFiles",Dir2[i],sep="/")) 
+  data <- read.csv(paste("Z:/Public/BOM_Stationdata/MergedUpToDateFiles",Dir2[i],sep="/")) 
   Dates <- paste(data$Year,data$Month,data$Day, sep="-")
   MaxT[[i]] <- window(zoo(data[,10],order.by=as.Date(Dates)), start = as.Date("1970-01-01"), end = as.Date("2010-12-31"))
   St_Rain[[i]]<- window(zoo(data[,6],order.by=as.Date(Dates)), start = as.Date("1970-01-01"), end = as.Date("2010-12-31"))  # use window() to cut the right time frame
 }
 
 saveRDS(MaxT, 'MaxT.Rds')
+saveRDS(St_Rain, 'St_Rain.Rds')
 
 # combine MaxT for all stations
 
 MaxT<-readRDS('MaxT.Rds')
-
+St_Rain<-readRDS('St_Rain.Rds')
 #checking empty columns
 t<-sapply(MaxT, function(x) {
   t <- table(is.na(x))
@@ -39,7 +40,22 @@ t<-sapply(MaxT, function(x) {
 })
 table(t==1)
 
-test<-do.call(merge, MaxT) #give me "Large zoo" not dataframe
+test<-do.call(merge, MaxT)
 
 names(test) <- Station_id[!is.na(t)]
 test2<-as.data.frame(test)
+
+# combine St_Rain for all stations
+s<-sapply(St_Rain, function(x) {
+  s <- table(is.na(x))
+  num_s <- s['TRUE']
+  ifelse(num_s, num_s/sum(s), 0)
+})
+table(s==1)
+
+test3<-do.call(merge, St_Rain) 
+
+names(test3) <- Station_id[!is.na(s)]
+#Error in `colnames<-`(`*tmp*`, value = c(72000L, 72089L, 61415L, 60140L,  : 
+#length of 'dimnames' [2] not equal to array extent
+test4<-as.data.frame(test3)
